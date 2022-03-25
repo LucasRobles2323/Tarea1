895b85b4ba1113bcffb183723cad06a1e7689a25
lucas.robles <you@example.com>
2022-03-25 15:04:14 -0300

    Este es un mensaje de estado

---
+++ Tarea1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+#include <stdbool.h>
+#include <ctype.h>
+
+typedef struct{
+    
+}Cancion;
+
+
+int main( ){
+
+    return 0;
+}
